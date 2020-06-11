b587d6bb5f29c0280e40ba80f33ce2c0a688025a
hooN <johnhpark11@gmail.com>
2020-06-11 10:49:54 +0900

    Google Login, Modal, Delete unused comments

--- my_site/settings.py
+++ my_site/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SITE_ID = 1
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -140,3 +139,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = '/blog/'
+
+SITE_ID = 1
--- my_site/urls.py
+++ my_site/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
     path('blog/', include('blog.urls')),
     path('accounts/', include('allauth.urls')),
 ]
