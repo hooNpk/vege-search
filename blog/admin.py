e010121755ce6088b9d2fd1d77c309ad36ac027d
hooN <johnhpark11@gmail.com>
2020-06-13 18:18:49 +0900

    test

--- blog/admin.py
+++ blog/admin.py
@@ -5,4 +5,4 @@ from .models import Post, Comment
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
-admin.site.register(Restaurant)
+#admin.site.register(Restaurant)
